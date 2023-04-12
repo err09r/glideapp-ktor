e8ed740b2acb50610d58b6d374f7a9c1279c1741
Raman Makarevich <raman.makarewicz@gmail.com>
2023-04-13 00:22:46 +0200

    Request only vehicles within given bounds

--- api/src/main/kotlin/com/apsl/glideapp/features/map/MapRoutes.kt
+++ api/src/main/kotlin/com/apsl/glideapp/features/map/MapRoutes.kt
@@ -2,18 +2,23 @@ package com.apsl.glideapp.features.map
 
 import CoordinatesBounds
 import io.ktor.server.routing.Route
-import io.ktor.server.websocket.receiveDeserialized
 import io.ktor.server.websocket.sendSerialized
 import io.ktor.server.websocket.webSocket
+import io.ktor.websocket.Frame
+import io.ktor.websocket.readText
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
 import org.koin.ktor.ext.inject
 
 fun Route.mapRoutes() {
     val mapController: MapController by inject()
     webSocket("map") {
-        while (true) {
+        for (frame in incoming) {
+            frame as? Frame.Text ?: continue
+
             val coordinatesBounds = runCatching {
-                receiveDeserialized<CoordinatesBounds>()
+                Json.decodeFromString<CoordinatesBounds>(frame.readText())
             }.getOrNull()
 
             if (coordinatesBounds != null) {
