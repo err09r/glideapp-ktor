48bb56b1cbcae0f718ebb5d734357ead286c50fb
Raman Makarevich <raman.makarewicz@gmail.com>
2023-04-21 20:42:07 +0200

    Update dependencies

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,12 +1,12 @@
 object Versions {
     const val kotlin = "1.8.20"
-    const val ktor = "2.2.4"
+    const val ktor = "2.3.0"
     const val gradleVersionsPlugin = "0.46.0"
     const val glideAppCommon = "0.0.7"
     const val koin = "3.4.0"
     const val exposed = "0.41.1"
     const val postgresql = "42.6.0"
-    const val logback = "1.4.6"
+    const val logback = "1.4.7"
     const val commonsCodec = "1.15"
 
     object Kotlin {
