090efbcfd61d5d4ade27e78c6789ba3b0336316f
Raman Makarevich <raman.makarewicz@gmail.com>
2023-09-11 13:44:25 +0200

    Change 'query' function signature

--- api/src/main/kotlin/com/apsl/glideapp/database/DatabaseFactory.kt
+++ api/src/main/kotlin/com/apsl/glideapp/database/DatabaseFactory.kt
@@ -10,6 +10,7 @@ import io.ktor.server.config.ApplicationConfig
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.Transaction
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.sql.transactions.transaction
 
@@ -31,5 +32,7 @@ object DatabaseFactory {
         }
     }
 
-    suspend fun <T> query(block: suspend () -> T): T = newSuspendedTransaction(Dispatchers.IO) { block() }
+    suspend fun <T> query(block: suspend Transaction.() -> T): T {
+        return newSuspendedTransaction(context = Dispatchers.IO, statement = block)
+    }
 }
