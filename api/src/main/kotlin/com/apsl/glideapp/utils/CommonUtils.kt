c645b5905a6257d8bbde86f0686f927176278127
Raman Makarevich <raman.makarewicz@gmail.com>
2023-09-29 19:02:49 +0200

    Refactor CommonUtils.kt

--- api/src/main/kotlin/com/apsl/glideapp/utils/CommonUtils.kt
+++ api/src/main/kotlin/com/apsl/glideapp/utils/CommonUtils.kt
@@ -5,7 +5,7 @@ import java.io.File
 
 fun Any.readFileFromResources(path: String): String {
     return try {
-        val uri = javaClass.classLoader.getResource(path)?.toURI()
+        val uri = this.javaClass.classLoader.getResource(path)?.toURI()
         checkNotNull(uri)
         File(uri).readText()
     } catch (e: Exception) {
