e1af2e0e043a32f8da52c23bcb7454a17e353f93
Roman Makarewicz <roma123makar@mail.ru>
2023-10-25 15:29:22 +0200

    Remove commented code in Http.kt

--- api/src/main/kotlin/com/apsl/glideapp/plugins/Http.kt
+++ api/src/main/kotlin/com/apsl/glideapp/plugins/Http.kt
@@ -5,12 +5,6 @@ import io.ktor.server.plugins.swagger.swaggerUI
 import io.ktor.server.routing.routing
 
 fun Application.configureHttp() {
-//    install(HttpsRedirect) {
-//        // The port to redirect to. By default 443, the default HTTPS port.
-//        sslPort = 443
-//        // 301 Moved Permanently, or 302 Found redirect.
-//        permanentRedirect = true
-//    }
     routing {
         swaggerUI(path = "api/docs", swaggerFile = "openapi/documentation.yaml")
     }
