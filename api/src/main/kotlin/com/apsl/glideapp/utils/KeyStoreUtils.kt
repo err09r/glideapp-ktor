bf8bbb059b3b75717c08a9346b956cee5c1dad4b
Raman Makarevich <raman.makarewicz@gmail.com>
2023-11-21 19:46:47 +0100

    Update KeyStoreUtils.kt

--- api/src/main/kotlin/com/apsl/glideapp/utils/KeyStoreUtils.kt
+++ api/src/main/kotlin/com/apsl/glideapp/utils/KeyStoreUtils.kt
@@ -1,23 +1,23 @@
 package com.apsl.glideapp.utils
 
-import io.ktor.network.tls.certificates.buildKeyStore
-import io.ktor.network.tls.certificates.saveToFile
+import io.ktor.network.tls.certificates.KeyType
+import io.ktor.network.tls.certificates.generateCertificate
 import java.io.File
 
 object KeyStoreUtils {
 
-    private const val CERTIFICATE_ALIAS = "alias"
-    private const val CERTIFICATE_PASSWORD = "password"
-    private const val FILE_PASSWORD = "password"
+    private const val KEY_ALIAS = "alias"
+    private const val KEY_PASSWORD = "password"
+    private const val JKS_PASSWORD = "password"
 
     fun createKeyStoreFile() {
         val keyStoreFile = File("build/keystore.jks")
-        val keyStore = buildKeyStore {
-            certificate(CERTIFICATE_ALIAS) {
-                password = CERTIFICATE_PASSWORD
-                domains = listOf("127.0.0.1", "0.0.0.0", "localhost")
-            }
-        }
-        keyStore.saveToFile(keyStoreFile, FILE_PASSWORD)
+        generateCertificate(
+            file = keyStoreFile,
+            keyAlias = KEY_ALIAS,
+            keyPassword = KEY_PASSWORD,
+            jksPassword = JKS_PASSWORD,
+            keyType = KeyType.Server
+        )
     }
 }
