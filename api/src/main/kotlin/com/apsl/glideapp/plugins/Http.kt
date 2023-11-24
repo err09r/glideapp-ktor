4e0cbd594f4907e1fe24ce80faaa60fbe69b2c30
Roman Makarewicz <roma123makar@mail.ru>
2023-11-24 11:07:23 +0100

    Disable 'HttpRedirect' plugin

--- api/src/main/kotlin/com/apsl/glideapp/plugins/Http.kt
+++ api/src/main/kotlin/com/apsl/glideapp/plugins/Http.kt
@@ -3,18 +3,16 @@
 package com.apsl.glideapp.plugins
 
 import io.ktor.server.application.Application
-import io.ktor.server.application.install
 import io.ktor.server.config.ApplicationConfig
-import io.ktor.server.plugins.httpsredirect.HttpsRedirect
 import io.ktor.server.plugins.swagger.swaggerUI
 import io.ktor.server.routing.routing
 
 fun Application.configureHttp() {
-    val applicationConfig: ApplicationConfig = environment.config
-    install(HttpsRedirect) {
-        sslPort = getSslPort(config = applicationConfig)
-        permanentRedirect = true
-    }
+//    val applicationConfig: ApplicationConfig = environment.config
+//    install(HttpsRedirect) {
+//        sslPort = getSslPort(config = applicationConfig)
+//        permanentRedirect = true
+//    }
     routing {
         swaggerUI(path = "api/docs", swaggerFile = "openapi/documentation.yaml")
     }
