787f63b687c9f0e575d74ec6bd90c43d017704ff
Roman Makarewicz <roma123makar@mail.ru>
2023-12-22 12:18:59 +0100

    Update username validation pattern

--- api/src/main/kotlin/com/apsl/glideapp/features/auth/RegisterFieldVerifier.kt
+++ api/src/main/kotlin/com/apsl/glideapp/features/auth/RegisterFieldVerifier.kt
@@ -5,7 +5,7 @@ object RegisterFieldVerifier {
     private val emptyException = Exception()
 
     fun verifyUsername(username: String): Result<Unit> {
-        val isValid = username.matches(Regex("^[a-z0-9_-]{4,20}\$"))
+        val isValid = username.matches(Regex("^[a-z0-9_.-]{4,20}\$"))
         return if (isValid) Result.success(Unit) else Result.failure(emptyException)
     }
 
