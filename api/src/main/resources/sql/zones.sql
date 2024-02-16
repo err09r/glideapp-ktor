1ffc29b2a2d8ab1baf284b4050640386c6aa69f1
Roman Makarewicz <raman.makarewicz@gmail.com>
2024-02-16 17:17:34 +0100

    Update .sql files

--- api/src/main/resources/sql/zones.sql
+++ api/src/main/resources/sql/zones.sql
@@ -9,18 +9,33 @@ $$
             VALUES (gen_random_uuid(), 1, 'SL-R-1', 0, now()::TIMESTAMP, now()::TIMESTAMP);
 
             INSERT INTO zones (id, code, title, type, created_at, updated_at)
-            VALUES (gen_random_uuid(), 3, 'SL-NP-1', 1, now()::TIMESTAMP, now()::TIMESTAMP);
+            VALUES (gen_random_uuid(), 100, 'SL-NP-1', 1, now()::TIMESTAMP, now()::TIMESTAMP);
 
             INSERT INTO zones (id, code, title, type, created_at, updated_at)
-            VALUES (gen_random_uuid(), 4, 'SL-NP-2', 1, now()::TIMESTAMP, now()::TIMESTAMP);
+            VALUES (gen_random_uuid(), 101, 'SL-NP-2', 1, now()::TIMESTAMP, now()::TIMESTAMP);
 
             INSERT INTO zones (id, code, title, type, created_at, updated_at)
-            VALUES (gen_random_uuid(), 5, 'SL-NP-3', 1, now()::TIMESTAMP, now()::TIMESTAMP);
+            VALUES (gen_random_uuid(), 102, 'SL-NP-3', 1, now()::TIMESTAMP, now()::TIMESTAMP);
+
+            INSERT INTO zones (id, code, title, type, created_at, updated_at)
+            VALUES (gen_random_uuid(), 103, 'SL-NP-4', 1, now()::TIMESTAMP, now()::TIMESTAMP);
+
+            INSERT INTO zones (id, code, title, type, created_at, updated_at)
+            VALUES (gen_random_uuid(), 104, 'SL-NP-5', 1, now()::TIMESTAMP, now()::TIMESTAMP);
 
 --             KO
 
             INSERT INTO zones (id, code, title, type, created_at, updated_at)
             VALUES (gen_random_uuid(), 2, 'KO-R-1', 0, now()::TIMESTAMP, now()::TIMESTAMP);
+
+            INSERT INTO zones (id, code, title, type, created_at, updated_at)
+            VALUES (gen_random_uuid(), 200, 'KO-NP-1', 1, now()::TIMESTAMP, now()::TIMESTAMP);
+
+            INSERT INTO zones (id, code, title, type, created_at, updated_at)
+            VALUES (gen_random_uuid(), 201, 'KO-NP-2', 1, now()::TIMESTAMP, now()::TIMESTAMP);
+
+            INSERT INTO zones (id, code, title, type, created_at, updated_at)
+            VALUES (gen_random_uuid(), 202, 'KO-NP-3', 1, now()::TIMESTAMP, now()::TIMESTAMP);
 
         END IF;
     END
