5bb915a31bc327398b67f7206ad73d5f85eb56a8
Roman Makarewicz <raman.makarewicz@gmail.com>
2024-06-28 16:08:31 +0200

    Enable HttpsRedirect plugin

--- api/src/main/kotlin/com/apsl/glideapp/api/plugins/Http.kt
+++ api/src/main/kotlin/com/apsl/glideapp/api/plugins/Http.kt
@@ -3,21 +3,24 @@
 package com.apsl.glideapp.api.plugins
 
 import io.ktor.server.application.Application
+import io.ktor.server.application.install
 import io.ktor.server.config.ApplicationConfig
+import io.ktor.server.plugins.httpsredirect.HttpsRedirect
 import io.ktor.server.plugins.swagger.swaggerUI
 import io.ktor.server.routing.routing
 
 fun Application.configureHttp() {
-//    val applicationConfig: ApplicationConfig = environment.config
-//    install(HttpsRedirect) {
-//        sslPort = getSslPort(config = applicationConfig)
-//        permanentRedirect = true
-//    }
+    val applicationConfig: ApplicationConfig = environment.config
+    install(HttpsRedirect) {
+        sslPort = applicationConfig.sslPort
+        permanentRedirect = true
+    }
     routing {
         swaggerUI(path = "api/docs", swaggerFile = "openapi/documentation.yaml")
     }
 }
 
-private fun getSslPort(config: ApplicationConfig): Int {
-    return config.property("ktor.deployment.sslPort").getString().toInt()
-}
+/**
+ * The HTTPS port the current application is running on, as defined by the configuration at start-up.
+ **/
+private val ApplicationConfig.sslPort: Int get() = property("ktor.deployment.sslPort").getString().toInt()
