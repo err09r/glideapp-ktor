947b3b7e71722529d249e2c8f3f7e8b35155d617
Roman Makarewicz <raman.makarewicz@gmail.com>
2024-10-06 19:43:52 +0200

    Create .sql files for SQLite and refactor existing files for PostgreSQL
    Signed-off-by: Roman Makarewicz <raman.makarewicz@gmail.com>

---
+++ api/src/main/resources/sql/sqlite/zones.sql
@@ -0,0 +1,17 @@
+-- SL
+
+INSERT INTO zones (id, code, title, type, created_at, updated_at)
+VALUES ('ec523b69-c3cf-4c34-aaec-000000000001', 1, 'SL-R-1', 0, datetime('now'), datetime('now')),
+       ('63dd61f7-0d3f-482f-944f-000000000001', 100, 'SL-NP-1', 1, datetime('now'), datetime('now')),
+       ('63dd61f7-0d3f-482f-944f-000000000002', 101, 'SL-NP-2', 1, datetime('now'), datetime('now')),
+       ('63dd61f7-0d3f-482f-944f-000000000003', 102, 'SL-NP-3', 1, datetime('now'), datetime('now')),
+       ('63dd61f7-0d3f-482f-944f-000000000004', 103, 'SL-NP-4', 1, datetime('now'), datetime('now')),
+       ('63dd61f7-0d3f-482f-944f-000000000005', 104, 'SL-NP-5', 1, datetime('now'), datetime('now'));
+
+-- KO
+
+INSERT INTO zones (id, code, title, type, created_at, updated_at)
+VALUES ('ec523b69-c3cf-4c34-aaec-000000000002', 2, 'KO-R-1', 0, datetime('now'), datetime('now')),
+       ('1432cca7-ac42-49d1-9ba2-000000000001', 200, 'KO-NP-1', 1, datetime('now'), datetime('now')),
+       ('1432cca7-ac42-49d1-9ba2-000000000002', 201, 'KO-NP-2', 1, datetime('now'), datetime('now')),
+       ('1432cca7-ac42-49d1-9ba2-000000000003', 202, 'KO-NP-3', 1, datetime('now'), datetime('now'));
