4061e4eeb9d151ef1747df1e6364a8f27c540dfb
Roman Makarewicz <raman.makarewicz@gmail.com>
2024-10-07 17:05:23 +0200

    Bump version
    Signed-off-by: Roman Makarewicz <raman.makarewicz@gmail.com>

--- build-logic/plugins/src/main/kotlin/Config.kt
+++ build-logic/plugins/src/main/kotlin/Config.kt
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 object Config {
     const val group = "com.apsl.glideapp"
-    const val version = "0.1.1"
+    const val version = "0.1.2"
     val javaVersion = JavaVersion.VERSION_11
     val jvmTarget = JvmTarget.JVM_11
 }
